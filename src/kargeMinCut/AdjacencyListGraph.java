6a32887ea6abb742ffff3e9badf5a0351113264d
Camellia Peng <camelliapeng02@gmail.com>
2016-09-27 01:53:12 -0700

    more specs

--- src/kargeMinCut/AdjacencyListGraph.java
+++ src/kargeMinCut/AdjacencyListGraph.java
@@ -1,14 +1,15 @@
 package kargeMinCut;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class AdjacencyListGraph{
     private Map<Vertex, LinkedList<Vertex>> graph = new HashMap<Vertex, LinkedList<Vertex>>(); 
+    private Random random = new Random(); 
     
     public AdjacencyListGraph(){};
     
@@ -24,19 +25,60 @@ public class AdjacencyListGraph{
         graph.get(b).add(a);
     }
     
+    public Vertex[] getRandomEdge(){
+        Vertex[] result = new Vertex[2];
+        
+        try{
+            Vertex start = getRandomVertex(); 
+            List<Vertex> neighbors = getAdjacentVertex(start);
+            
+            while(neighbors.size() < 1){
+                start = getRandomVertex(); 
+            }
+            
+            Vertex end = neighbors.get(random.nextInt(neighbors.size()));
+            
+            result[0] = start;
+            result[1] = end;
+            
+            
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+    
+    public void contractEdge(Vertex v0, Vertex v1){
+        // TODO: implement
+    }
+    
+    private void removeSelfLoop(Vertex v){
+        // TODO: implement
+    }
+    
     public Set<Vertex> getVertices(){
         return graph.keySet(); 
     }
     
-    public List<Vertex> getAdjacentVertex(Vertex a) throws NoSuchVertexException{
+    public int getVerticesCount(){
+        return this.getVertices().size();
+    }
+    
+    public List<Vertex> getAdjacentVertex(Vertex a) throws NoSuchVertexException{ // testing only, should make private later
         if(!graph.containsKey(a)){
             throw new NoSuchVertexException();
         }
         
-        return Collections.unmodifiableList(graph.get(a));
+        return graph.get(a);
     }
     
-    class NoSuchVertexException extends Exception{
+    public Vertex getRandomVertex(){ // TODO: make private later
+        Vertex[] vertexArr = getVertices().toArray(new Vertex[0]);
+        return vertexArr[random.nextInt(vertexArr.length)]; 
+    }
+    
+    private class NoSuchVertexException extends Exception{
         private static final long serialVersionUID = 1L;
         
         public NoSuchVertexException(String msg){
--- src/kargeMinCut/Algorithms.java
+++ src/kargeMinCut/Algorithms.java
@@ -1,5 +1,6 @@
 package kargeMinCut;
 
+import java.util.List;
 import java.util.Set;
 
 public class Algorithms {
@@ -9,16 +10,25 @@ public class Algorithms {
         Vertex a = new Vertex("a");
         Vertex b = new Vertex("b");
         Vertex c = new Vertex("c");
+        Vertex d = new Vertex("d");
+        Vertex e = new Vertex("e");
         
         graph.addVertex(a);
         graph.addVertex(b);
         graph.addVertex(c);
+        graph.addVertex(d);
+        graph.addVertex(e);
         
         try {
             graph.addEdge(a, b);
             graph.addEdge(a, c);
-        } catch(Exception e){
-            e.printStackTrace();
+            graph.addEdge(b, c);
+            graph.addEdge(d, e);
+            graph.addEdge(a, e);
+            graph.addEdge(b, c);
+            graph.addEdge(e, a);
+        } catch(Exception exception){
+            exception.printStackTrace();
         }
         
         Set<Vertex> vertices = graph.getVertices();
@@ -27,9 +37,13 @@ public class Algorithms {
         for(Vertex v : vertices){
             try {
                 System.out.println(v.getLabel() + ": " + graph.getAdjacentVertex(v));
-            } catch(Exception e){
-                e.printStackTrace();
+            } catch(Exception exception){
+                exception.printStackTrace();
             }
         }
+        
+        Vertex[] randEdge = graph.getRandomEdge();
+        
+        System.out.println("random edge vertex: " + randEdge[0] + " - " + randEdge[1]);
     }
 }
