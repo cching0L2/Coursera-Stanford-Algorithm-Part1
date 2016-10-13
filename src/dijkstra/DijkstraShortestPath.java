bb978e9540e53804f7ad6ea3e41f192c485b209b
Camellia Peng <camelliapeng02@gmail.com>
2016-10-13 01:35:57 -0700

    dijkstra shortest path complete

--- src/dijkstra/DijkstraShortestPath.java
+++ src/dijkstra/DijkstraShortestPath.java
@@ -7,12 +7,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class DijkstraShortestPath{
+    private static boolean USE_SMALL = false; 
+    private static int NO_PATH = 1000000; 
     private static List<LinkedList<AdjListElement>> graph = new ArrayList<LinkedList<AdjListElement>>(); 
     
     private DijkstraShortestPath(){}
     
     private static class AdjListElement{
-        int destNode, weight; 
+        public int destNode, weight; 
         
         public AdjListElement(int destNode, int weight){
             this.destNode = destNode; 
@@ -24,6 +26,61 @@ public class DijkstraShortestPath{
         }
     }
     
+    private static void getShortestPath(){
+        int[] shortestFromSrc = new int[graph.size()]; // shortestFromSrc[0] is a dummy
+        List<Integer> processed = new ArrayList<Integer>(); 
+        List<Integer> unprocessed = new ArrayList<Integer>(); 
+        
+        for(int i = 1; i < graph.size(); i++){
+            unprocessed.add(i);
+        }
+        
+        processed.add(unprocessed.remove(0)); // start from 1
+        
+        while(!unprocessed.isEmpty()){
+            
+            int lowestSoFarNode = -1; 
+            int lowestSoFarScore = Integer.MAX_VALUE; 
+            
+            for(Integer v : processed){
+                List<AdjListElement> adjacent = graph.get(v);
+                for(AdjListElement w : adjacent){
+                    if(unprocessed.contains(w.destNode)){
+                        int score = shortestFromSrc[v] + w.weight; 
+                        if(score < lowestSoFarScore){
+                            lowestSoFarScore = score; 
+                            lowestSoFarNode = w.destNode; 
+                        }
+                    }
+                }
+            }
+            
+            if(lowestSoFarNode == -1){
+                System.out.println("graph not connected");
+                
+                while(!unprocessed.isEmpty()){
+                    int unprocessedNode = unprocessed.remove(0);
+                    shortestFromSrc[unprocessedNode] = NO_PATH;
+                }
+                
+                break; 
+            }
+            
+            shortestFromSrc[lowestSoFarNode] = lowestSoFarScore; 
+            processed.add(lowestSoFarNode); 
+            unprocessed.remove(new Integer(lowestSoFarNode));
+            
+        }
+        
+        System.out.println("shortest from src: ");
+        for(int i = 1; i < shortestFromSrc.length; i++){
+            System.out.println("to "+ i +": " + shortestFromSrc[i]);
+        }
+        System.out.println();
+        
+        
+    }
+
     private static void printGraph(){
         int counter = 0; 
         
@@ -44,7 +101,7 @@ public class DijkstraShortestPath{
     }
     
     private static void makeGraph(){
-        String filename = "input/pa5.txt"; 
+        String filename = USE_SMALL ? "input/pa5-small.txt" : "input/pa5.txt"; 
         
         try{
             BufferedReader br = new BufferedReader(new FileReader(filename)); 
@@ -75,6 +132,6 @@ public class DijkstraShortestPath{
     
     public static void main(String[] args){
         makeGraph(); 
-        printGraph(); 
+        getShortestPath(); 
     }
 }
